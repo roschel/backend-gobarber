27b634f366bdaaadfbb02198ce18f29460112ccc
roschel <joaomroschel@gmail.com>
2020-12-22 20:31:42 -0300

    user and appointment relation

--- src/database/migrations/1608673404737-CreateAppointments.ts
+++ src/database/migrations/1608673404737-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1608673404737
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1608676881924-CreateUsers.ts
+++ src/database/migrations/1608676881924-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1608676881924 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
