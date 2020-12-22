a264a7dabc63e85e8302ac29e9eb4d148fdb6e7e
roschel <joaomroschel@gmail.com>
2020-12-22 15:17:20 -0300

    prettier and debug

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express, { request, response } from 'express';
-import routes from './routes';
+import express, { request, response } from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello gostack' }));
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log("🚀 Server started on port 3333!");
 });
