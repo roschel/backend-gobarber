d56a0cb5a038f9edca375dc0615bb5120f9067f6
roschel <joaomroschel@gmail.com>
2020-12-22 17:41:14 -0300

    appointment model and repository

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
