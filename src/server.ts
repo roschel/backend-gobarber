db6237b885162c97d8bc43134c110b7b9c348a68
roschel <joaomroschel@gmail.com>
2020-12-23 19:08:23 -0300

    :wrench: cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -9,25 +10,28 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error',
     });
-  }
-
-  console.log(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
