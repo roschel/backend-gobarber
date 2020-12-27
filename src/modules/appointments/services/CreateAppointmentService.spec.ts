7f9f8283295d4884682e7f5a7f91134ea4ec1cd8
roschel <joaomroschel@gmail.com>
2020-12-27 20:14:09 -0300

    fix test to cover two appointments in the same date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,5 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12121132231');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {});
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 11, 27, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '3333',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '3333',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
